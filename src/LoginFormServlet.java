e5c5b43e20cf324c1ae4019e6b0e98bcd6c54c00
Jayden Le <khangtl3@uci.edu>
2023-10-17 13:00:51 -0700

    adding login page

---
+++ src/LoginFormServlet.java
@@ -0,0 +1,121 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import services.MoviesService;
+import utils.ServletUtils;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// Declaring a WebServlet called StarsServlet, which maps to url "/api/stars"
+@WebServlet(name = "LoginFormServlet", urlPatterns = "/api/login")
+public class LoginFormServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    // Create a dataSource which registered in web.
+    private DataSource dataSource;
+
+    private MoviesService service;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/json"); // Response mime type
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        // Get a connection from dataSource and let resource manager close the connection after usage.
+        try  {
+            JsonArray jsonArray = service.getMoviesArray(dataSource);
+
+            // Log to localhost log
+            request.getServletContext().log("getting " + jsonArray.size() + " results");
+
+            // Write JSON string to output
+            out.write(jsonArray.toString());
+            // Set response status to 200 (OK)
+            response.setStatus(200);
+
+        } catch (Exception e) {
+
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+
+        // Always remember to close db connection after usage. Here it's done by try-with-resources
+
+    }
+
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        String email = request.getParameter("email");
+//        String password = request.getParameter("password");
+//        System.out.println("email: " + email);
+//        System.out.println("password: " + password);
+//        response.setContentType("application/json"); // Response mime type
+//        PrintWriter out = response.getWriter();
+//
+//        try {
+//            JsonObject jsonObject = new JsonObject();
+//            jsonObject.addProperty("email", email);
+//            jsonObject.addProperty("password", password);
+//
+////            JsonObject jsonObject = service.login(dataSource, username, password);
+//            out.write(jsonObject.toString());
+//            response.setStatus(200);
+//        } catch (Exception e) {
+//            JsonObject jsonObject = new JsonObject();
+//            jsonObject.addProperty("errorMessage", e.getMessage());
+//            out.write(jsonObject.toString());
+//            response.setStatus(500);
+//        } finally {
+//            out.close();
+//        }
+
+        //get username and password
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        //check if username and password are valid from mysql
+
+
+
+
+
+        //if valid, create a session
+//        request.getSession().setAttribute("user", new User(username));
+
+
+        response.sendRedirect(ServletUtils.getBaseUrl(request) + "/index.html");
+    }
+}
