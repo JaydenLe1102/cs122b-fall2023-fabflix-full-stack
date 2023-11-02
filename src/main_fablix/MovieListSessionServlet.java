15746b16542581131cfdb72c2b068aa41b752ba7
Jayden Le <khangtl3@uci.edu>
2023-11-02 15:16:33 -0700

    rename and refactor fablix api servlet

--- src/main_fablix/MovieListSessionServlet.java
+++ src/main_fablix/MovieListSessionServlet.java
@@ -24,90 +24,90 @@ import java.io.PrintWriter;
 import static utils.ServletUtils.checkLogin;
 
 // Declaring a WebServlet called StarsServlet, which maps to url "/api/stars"
-@WebServlet(name = "main_fablix.MovieListSessionServlet", urlPatterns = "/api/movielist")
+@WebServlet(name = "MovieListSessionServlet", urlPatterns = "/api/movielist")
 public class MovieListSessionServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    // Create a dataSource which registered in web.
-    private DataSource dataSource;
+	// Create a dataSource which registered in web.
+	private DataSource dataSource;
 
-    public void init(ServletConfig config) {
-        try {
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+	public void init(ServletConfig config) {
+		try {
+			dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
 
-        } catch (NamingException e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     * response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        checkLogin(request, response);
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		checkLogin(request, response);
 
-        response.setContentType("application/json"); // Response mime type
+		response.setContentType("application/json"); // Response mime type
 
-        PrintWriter out = response.getWriter();
+		PrintWriter out = response.getWriter();
 
-        try {
+		try {
 
-            HttpSession session = request.getSession(true);
+			HttpSession session = request.getSession(true);
 
-            Boolean isBrowsed = (Boolean) session.getAttribute("isBrowsed");
-            Boolean isSearch = (Boolean) session.getAttribute("isSearch");
+			Boolean isBrowsed = (Boolean) session.getAttribute("isBrowsed");
+			Boolean isSearch = (Boolean) session.getAttribute("isSearch");
 
-            Integer page_number = (Integer) session.getAttribute("page_number") != null
-                    ? (Integer) session.getAttribute("page_number")
-                    : 1;
-            Integer page_size = (Integer) session.getAttribute("page_size") != null
-                    ? (Integer) session.getAttribute("page_size")
-                    : 10;
+			Integer page_number = (Integer) session.getAttribute("page_number") != null
+					? (Integer) session.getAttribute("page_number")
+					: 1;
+			Integer page_size = (Integer) session.getAttribute("page_size") != null
+					? (Integer) session.getAttribute("page_size")
+					: 10;
 
-            Integer sort_option = (Integer) session.getAttribute("sort_option") != null
-                    ? (Integer) session.getAttribute("sort_option")
-                    : 0;
+			Integer sort_option = (Integer) session.getAttribute("sort_option") != null
+					? (Integer) session.getAttribute("sort_option")
+					: 0;
 
-            System.out.println("page_number: " + page_number);
-            System.out.println("page_size: " + page_size);
-            System.out.println("isBrowsed: " + isBrowsed);
-            System.out.println("isSearch: " + isSearch);
+			System.out.println("page_number: " + page_number);
+			System.out.println("page_size: " + page_size);
+			System.out.println("isBrowsed: " + isBrowsed);
+			System.out.println("isSearch: " + isSearch);
 
-            JsonObject responseJsonObject = new JsonObject();
+			JsonObject responseJsonObject = new JsonObject();
 
-            responseJsonObject.addProperty("page_number", page_number);
-            responseJsonObject.addProperty("page_size", page_size);
-            responseJsonObject.addProperty("sort_option", sort_option);
-            responseJsonObject.addProperty("isBrowsed", isBrowsed);
-            responseJsonObject.addProperty("isSearch", isSearch);
+			responseJsonObject.addProperty("page_number", page_number);
+			responseJsonObject.addProperty("page_size", page_size);
+			responseJsonObject.addProperty("sort_option", sort_option);
+			responseJsonObject.addProperty("isBrowsed", isBrowsed);
+			responseJsonObject.addProperty("isSearch", isSearch);
 
-            responseJsonObject.addProperty("browse_genre", (String) session.getAttribute("browse_genre"));
-            responseJsonObject.addProperty("browse_title", (String) session.getAttribute("browse_title"));
+			responseJsonObject.addProperty("browse_genre", (String) session.getAttribute("browse_genre"));
+			responseJsonObject.addProperty("browse_title", (String) session.getAttribute("browse_title"));
 
-            responseJsonObject.addProperty("search_title", (String) session.getAttribute("search_title"));
-            responseJsonObject.addProperty("search_year", (String) session.getAttribute("search_year"));
-            responseJsonObject.addProperty("search_director", (String) session.getAttribute("search_director"));
-            responseJsonObject.addProperty("search_star", (String) session.getAttribute("search_star"));
+			responseJsonObject.addProperty("search_title", (String) session.getAttribute("search_title"));
+			responseJsonObject.addProperty("search_year", (String) session.getAttribute("search_year"));
+			responseJsonObject.addProperty("search_director", (String) session.getAttribute("search_director"));
+			responseJsonObject.addProperty("search_star", (String) session.getAttribute("search_star"));
 
-            out.write(responseJsonObject.toString());
-            response.setStatus(200);
+			out.write(responseJsonObject.toString());
+			response.setStatus(200);
 
-        } catch (Exception e) {
+		} catch (Exception e) {
 
-            // Write error message JSON object to output
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("errorMessage", e.getMessage());
-            out.write(jsonObject.toString());
+			// Write error message JSON object to output
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("errorMessage", e.getMessage());
+			out.write(jsonObject.toString());
 
-            // Set response status to 500 (Internal Server Error)
-            response.setStatus(500);
-        } finally {
-            out.close();
-        }
+			// Set response status to 500 (Internal Server Error)
+			response.setStatus(500);
+		} finally {
+			out.close();
+		}
 
-        // Always remember to close db connection after usage. Here it's done by
-        // try-with-resources
+		// Always remember to close db connection after usage. Here it's done by
+		// try-with-resources
 
-    }
+	}
 }
