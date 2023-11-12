44f3433da2f87971511917bab16f1e5073c186fe
Jayden Le <khangtl3@uci.edu>
2023-11-12 15:56:52 -0800

    update cred

--- src/script/SAXParser/src/main/java/MainParser.java
+++ src/script/SAXParser/src/main/java/MainParser.java
@@ -44,7 +44,7 @@ public class MainParser {
     private static ConnectionPool createConnectionPool() {
         String jdbcUrl = "jdbc:mysql://localhost:3306/moviedb";
         String username = "mytestuser";
-        String password = "My6$Password";
+        String password = "12345";
         int maxConnections = 10; // Adjust as needed
 
         return new HikariConnectionPool(jdbcUrl, username, password, maxConnections);
