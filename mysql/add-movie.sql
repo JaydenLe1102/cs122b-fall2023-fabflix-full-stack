603829b6b30833a3023343493360881c5400c369
Jayden Le <khangtl3@uci.edu>
2023-11-12 18:22:36 -0800

    update add-movie

--- mysql/add-movie.sql
+++ mysql/add-movie.sql
@@ -45,58 +45,56 @@ BEGIN
     DECLARE v_movieId VARCHAR(10);
     DECLARE v_nextStarId VARCHAR(10);
     DECLARE v_success INT DEFAULT 0;
-		
+
 
     -- Check if the movie already exists
     IF EXISTS (SELECT 1 FROM moviedb.movies WHERE title = p_title AND year = p_year) THEN
-        SELECT 0 as result, NULL as movieId, NULL as starId, NULL as genreId; 
-        LEAVE;
-    END IF;
-
-
-    START TRANSACTION;
-
-    SET v_movieId = GetNextMovieId();
-
-    SET v_nextStarId = GetNextStarId();
-
-    IF p_birthYear IS NULL THEN
-        SELECT id INTO v_starId FROM moviedb.stars WHERE name = p_starName AND birthYear IS NULL LIMIT 1;
+        SELECT 0 as result, NULL as movieId, NULL as starId, NULL as genreId;
     ELSE
-        SELECT id INTO v_starId FROM moviedb.stars WHERE name = p_starName AND birthYear = p_birthYear LIMIT 1;
-    END IF;
+        START TRANSACTION;
 
-    IF v_starId IS NULL THEN
+        SET v_movieId = GetNextMovieId();
+
+        SET v_nextStarId = GetNextStarId();
+
         IF p_birthYear IS NULL THEN
-            INSERT INTO moviedb.stars (id, name, birthYear) VALUES (v_nextStarId, p_starName, NULL);
+            SELECT id INTO v_starId FROM moviedb.stars WHERE name = p_starName AND birthYear IS NULL LIMIT 1;
         ELSE
-            INSERT INTO moviedb.stars (id, name, birthYear) VALUES (v_nextStarId, p_starName, p_birthYear);
+            SELECT id INTO v_starId FROM moviedb.stars WHERE name = p_starName AND birthYear = p_birthYear LIMIT 1;
         END IF;
-				SET v_success = v_success + ROW_COUNT();
-        SET v_starId = v_nextStarId;
+
+        IF v_starId IS NULL THEN
+            IF p_birthYear IS NULL THEN
+                INSERT INTO moviedb.stars (id, name, birthYear) VALUES (v_nextStarId, p_starName, NULL);
+            ELSE
+                INSERT INTO moviedb.stars (id, name, birthYear) VALUES (v_nextStarId, p_starName, p_birthYear);
+            END IF;
+                    SET v_success = v_success + ROW_COUNT();
+            SET v_starId = v_nextStarId;
+        END IF;
+
+        SELECT id INTO v_genreId FROM moviedb.genres WHERE name = p_genreName LIMIT 1;
+
+        IF v_genreId IS NULL THEN
+            INSERT INTO moviedb.genres (id, name) VALUES (NULL, p_genreName);
+                    SET v_success = v_success + ROW_COUNT();
+            SET v_genreId = LAST_INSERT_ID();
+        END IF;
+
+        INSERT INTO moviedb.movies (id, title, year, director) VALUES (v_movieId, p_title, p_year, p_director);
+            SET v_success = v_success + ROW_COUNT();
+        INSERT INTO moviedb.stars_in_movies (starId, movieId) VALUES (v_starId, v_movieId);
+            SET v_success = v_success + ROW_COUNT();
+        INSERT INTO moviedb.genres_in_movies (genreId, movieId) VALUES (v_genreId, v_movieId);
+            SET v_success = v_success + ROW_COUNT();
+
+        IF v_success > 2 AND v_success <6 THEN
+            COMMIT;
+        ELSE
+            ROLLBACK;
+        END IF;
+
+        SELECT v_success as result, v_movieId as movieId, v_starId as starId, v_genreId as genreId;
     END IF;
-
-    SELECT id INTO v_genreId FROM moviedb.genres WHERE name = p_genreName LIMIT 1;
-
-    IF v_genreId IS NULL THEN
-        INSERT INTO moviedb.genres (id, name) VALUES (NULL, p_genreName);
-				SET v_success = v_success + ROW_COUNT();
-        SET v_genreId = LAST_INSERT_ID();
-    END IF;
-
-    INSERT INTO moviedb.movies (id, title, year, director) VALUES (v_movieId, p_title, p_year, p_director);
-		SET v_success = v_success + ROW_COUNT();
-    INSERT INTO moviedb.stars_in_movies (starId, movieId) VALUES (v_starId, v_movieId);
-		SET v_success = v_success + ROW_COUNT();
-    INSERT INTO moviedb.genres_in_movies (genreId, movieId) VALUES (v_genreId, v_movieId);
-		SET v_success = v_success + ROW_COUNT();
-		
-    IF v_success > 2 AND v_success <6 THEN
-        COMMIT;
-    ELSE
-        ROLLBACK;
-    END IF;
-
-    SELECT v_success as result, v_movieId as movieId, v_starId as starId, v_genreId as genreId;
 END //
 DELIMITER ;
